5ffbd8383de8f63c80b072ed83e3da41fbdaa89a
Michał Startek <michal.startek.uw@gmail.com>
2024-10-15 18:40:28 +0200

    Improve plotmaking

--- plotting/performance_plot.py
+++ plotting/performance_plot.py
@@ -1,51 +1,55 @@
 import subprocess, itertools, json
 from tqdm import tqdm
+import socket
+from random import shuffle
+import pandas as pd
 
 def run_performance_script(test_size):
     outputs = []
-    result = subprocess.run(['../performance', str(test_size), "a"], capture_output=True, text=True)
+    file_path = "test.dat"
+    if socket.gethostname() == "utm":
+        file_path = "/home/mist/test.dat"
+    result = subprocess.run(['../performance', str(test_size), file_path], capture_output=True, text=True)
     json_text = '[' + result.stdout.split('[')[1]
     results = json.loads(json_text)
     res = {t['name']: t['duration'] for t in results}
+    res['test_size'] = test_size
+    del res['item_count']
     return res
 
 if __name__ == "__main__":
-    no_repeats = 5  # Change this to the number of times you want to run the script
-    test_sizes = [1, 3, 10, 33, 100, 333, 1000, 3333, 10000, 33333, 100000, 333333, 1000000, 3333333, 10000000]
+    no_repeats = 20  # Change this to the number of times you want to run the script
+    test_sizes = [1, 3, 10, 33, 100, 333, 1000, 3333, 10000, 33333, 100000, 333333, 1000000, 3333333, 10000000, 33333333, 100000000]#, 333333333, 1000000000]
     results = []
-    for repeat_num, test_size in tqdm(itertools.product(range(no_repeats), test_sizes), total=no_repeats*len(test_sizes)):
+    loop_args = list(itertools.product(range(no_repeats), test_sizes))
+    shuffle(loop_args)
+    for repeat_num, test_size in tqdm(loop_args):
         result = run_performance_script(test_size)
+        #print(result)
         results.append(result)
-    #print(results)
-
-    from collections import defaultdict
-    grouped = defaultdict(list)
-    for result in results:
-        item_count = result['item_count']
-        for key, value in result.items():
-            grouped[(item_count, key)].append(value)
-
-    averages = {key: sum(values) / len(values) for key, values in grouped.items()}
-    # drop the item_count key
-    averages = {key: value for key, value in averages.items() if key[1] != 'item_count'}
-    x_axis = test_sizes
-    y_axes = defaultdict(list)
-    for key, value in averages.items():
-        y_axes[key[1]].append(value)
-
-    print(x_axis)
-    print(y_axes)
+    results = pd.DataFrame(results)
+    print(results)
+    results = results.groupby('test_size').mean().reset_index()
+    print(results)
 
     import matplotlib.pyplot as plt
     import numpy as np
+    import matplotlib.cm as cm
 
     # lines plot, one line per algorithm
     # make the one named "std::vector" really stand out
-    for key, values in y_axes.items():
-        if key == 'std::vector':
-            plt.plot(x_axis, values, label=key, linewidth=5)
+    # use rainbow color map
+
+    colors = cm.rainbow(np.linspace(0, 1, len(results.columns) - 1))
+    color_dict = {colname: color for colname, color in zip(results.columns[1:], colors)}
+
+    for colname in results.columns:
+        if colname == 'test_size':
+            continue
+        if colname == 'std::vector':
+            plt.plot(results.test_size, results[colname], label=colname, linewidth=5, color='black')
         else:
-            plt.plot(x_axis, values, label=key)
+            plt.plot(results.test_size, results[colname], label=colname, color=color_dict[colname])
     plt.legend()
     plt.xscale('log')
     plt.yscale('log')
